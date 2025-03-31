b54a5a386e5b65b665f8dd9c55c34c087802cbec
ThuanNgcodelor <nguyentrungthuan417@gmail.com>
2025-03-31 22:16:57 +0700

    full model

--- obj/Debug/net8.0/CP2496H07Group1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CP2496H07Group1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CP2496H07Group1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8af1fa78897c0e15462c1ca65cea7730e464bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2021b8428a0e0c2afd58e8cfc30b5e46412d4086")]
 [assembly: System.Reflection.AssemblyProductAttribute("CP2496H07Group1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CP2496H07Group1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
